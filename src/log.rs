afa9326c819c1fb80795a8a81c2431344c7640d1
poma <Semenov.Roman@mail.ru>
2019-03-24 11:43:42 +0300

    fix log macros

--- src/log.rs
+++ src/log.rs
@@ -38,19 +38,19 @@ cfg_if! {
         }
     } else {
         macro_rules! log {
-            ($($t:tt)*) => (println!("{}", &format_args!($($t)*).to_string()))
+            ($($t:tt)*) => (println!($($t)*))
         }
 
         macro_rules! elog {
-            ($($t:tt)*) => (println!("{}", &format_args!($($t)*).to_string()))
+            ($($t:tt)*) => (eprintln!($($t)*))
         }
 
         macro_rules! log_verbose {
-            ($($t:tt)*) => (if $crate::verbose_flag() { println!("{}", &format_args!($($t)*).to_string()) })
+            ($($t:tt)*) => (if $crate::verbose_flag() { println!($($t)*) })
         }
 
         macro_rules! elog_verbose {
-            ($($t:tt)*) => (if $crate::verbose_flag() { println!("{}", &format_args!($($t)*).to_string()) })
+            ($($t:tt)*) => (if $crate::verbose_flag() { eprintln!($($t)*) })
         }
 
         pub struct Stopwatch {
