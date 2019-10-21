ad6dd9503871295bb7e80848aa4b4f5fc0fdb48f
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2019-10-21 21:25:01 +0330

    catch the panic thrown from Platform::list

--- src/gpu/utils.rs
+++ src/gpu/utils.rs
@@ -1,16 +1,20 @@
 use ocl::{Device, Platform};
+use std::panic;
 use super::error::{GPUResult, GPUError};
 
 pub const GPU_NVIDIA_PLATFORM_NAME : &str = "NVIDIA CUDA";
 pub const CPU_INTEL_PLATFORM_NAME : &str = "Intel(R) CPU Runtime for OpenCL(TM) Applications";
 
 pub fn get_devices(platform_name: &str) -> GPUResult<Vec<Device>> {
-    let platform = Platform::list().into_iter().find(|&p|
-        match p.name() {
-            Ok(p) => p == platform_name,
-            Err(_) => false
-        });
-    if platform.is_none() { return Err(GPUError {msg: "GPU platform not found!".to_string()}); }
-    let devices = Device::list_all(platform.unwrap())?;
-    return Ok(devices);
+    match panic::catch_unwind(|| {
+        let platform = Platform::list().into_iter().find(|&p|
+            match p.name() {
+                Ok(p) => p == platform_name,
+                Err(_) => false
+            });
+        Device::list_all(platform.unwrap()).unwrap()
+    }) {
+        Ok(devs) => Ok(devs),
+        Err(_) => Err(GPUError {msg: "GPU platform not found!".to_string()})
+    }
 }
