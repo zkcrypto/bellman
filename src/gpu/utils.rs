8ee0ec401f8dda32f34951145278c6866dc2ee9f
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2019-10-29 03:40:43 +0330

    use forked ocl with a calm Platform::list()

--- src/gpu/utils.rs
+++ src/gpu/utils.rs
@@ -1,21 +1,18 @@
 use ocl::{Device, Platform};
-use std::panic;
 use super::error::{GPUResult, GPUError};
 
 pub const GPU_NVIDIA_PLATFORM_NAME : &str = "NVIDIA CUDA";
 pub const CPU_INTEL_PLATFORM_NAME : &str = "Intel(R) CPU Runtime for OpenCL(TM) Applications";
 
 pub fn get_devices(platform_name: &str) -> GPUResult<Vec<Device>> {
-    match panic::catch_unwind(|| {
-        let platform = Platform::list().into_iter().find(|&p|
-            match p.name() {
-                Ok(p) => p == platform_name,
-                Err(_) => false
-            });
-        Device::list_all(platform.unwrap()).unwrap()
-    }) {
-        Ok(devs) => Ok(devs),
-        Err(_) => Err(GPUError {msg: "GPU platform not found!".to_string()})
+    let platform = Platform::list()?.into_iter().find(|&p|
+        match p.name() {
+            Ok(p) => p == platform_name,
+            Err(_) => false
+        });
+    match platform {
+        Some(p) => Ok(Device::list_all(p)?),
+        None => Err(GPUError {msg: "GPU platform not found!".to_string() })
     }
 }
 
