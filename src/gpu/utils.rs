9f9bce5810505764eefd446e0d688e2f30bb0956
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2019-11-04 23:29:11 +0330

    feat: add BELLMAN_NO_GPU flag

--- src/gpu/utils.rs
+++ src/gpu/utils.rs
@@ -1,10 +1,16 @@
 use ocl::{Device, Platform};
 use super::error::{GPUResult, GPUError};
+use std::env;
 
 pub const GPU_NVIDIA_PLATFORM_NAME : &str = "NVIDIA CUDA";
 pub const CPU_INTEL_PLATFORM_NAME : &str = "Intel(R) CPU Runtime for OpenCL(TM) Applications";
 
 pub fn get_devices(platform_name: &str) -> GPUResult<Vec<Device>> {
+
+    if env::var("BELLMAN_NO_GPU").is_ok() {
+        return Err(GPUError {msg: "GPU accelerator is disabled!".to_string()})
+    }
+
     let platform = Platform::list()?.into_iter().find(|&p|
         match p.name() {
             Ok(p) => p == platform_name,
