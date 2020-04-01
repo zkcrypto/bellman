6e50dc4ee2407cdd31db749bd8c6cc240169c251
Konstantce <kopanarin@yandex.ru>
2020-04-01 16:26:30 +0300

    Pred-release

--- src/redshift/redshift/mod.rs
+++ src/redshift/redshift/mod.rs
@@ -8,4 +8,3 @@ pub mod generators;
 pub mod prover;
 pub mod verifier;
 pub mod tests;
-//pub mod verifying_circuit;
--- src/redshift/redshift/verifying_circuit/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod common;
-pub mod rescue_gadget;
-pub mod transplilation_check;
-pub mod boolean;
-//pub mod merklee_tree_gadget;
