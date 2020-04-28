bae2f1e287f74e79c552c7f6b4c11acd50f100d0
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-04-28 17:28:26 +0430

    use bit exponent ops that are provided in ff-cl-gen

--- src/gpu/sources.rs
+++ src/gpu/sources.rs
@@ -1,4 +1,3 @@
-use ff::PrimeField;
 use ff_cl_gen as ffgen;
 use paired::Engine;
 
@@ -7,31 +6,9 @@ use paired::Engine;
 // generate OpenCL codes given different PrimeFields and curves.
 
 static FFT_SRC: &str = include_str!("fft/fft.cl");
-static EXP_SRC: &str = include_str!("multiexp/exp.cl");
 static FIELD2_SRC: &str = include_str!("multiexp/field2.cl");
 static EC_SRC: &str = include_str!("multiexp/ec.cl");
 static MULTIEXP_SRC: &str = include_str!("multiexp/multiexp.cl");
-
-/// Divide anything into 64bit chunks
-fn limbs_of<T>(value: &T) -> &[u64] {
-    unsafe {
-        std::slice::from_raw_parts(
-            value as *const T as *const u64,
-            std::mem::size_of::<T>() / 8,
-        )
-    }
-}
-
-fn exponent<F>(name: &str) -> String
-where
-    F: PrimeField,
-{
-    return format!(
-        "{}\n{}\n",
-        format!("#define {}_LIMBS {}", name, limbs_of(&F::one()).len()),
-        String::from(EXP_SRC).replace("EXPONENT", name)
-    );
-}
 
 fn field2(field2: &str, field: &str) -> String {
     return String::from(FIELD2_SRC)
@@ -61,16 +38,15 @@ where
     E: Engine,
 {
     return String::from(format!(
-        "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}",
+        "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}",
         ffgen::common(),
         ffgen::field::<E::Fr>("Fr"),
         fft("Fr"),
-        exponent::<E::Fr>("Exp"),
         ffgen::field::<E::Fq>("Fq"),
         ec("Fq", "G1"),
-        multiexp("G1", "Exp"),
+        multiexp("G1", "Fr"),
         field2("Fq2", "Fq"),
         ec("Fq2", "G2"),
-        multiexp("G2", "Exp")
+        multiexp("G2", "Fr")
     ));
 }
