414c4f2e8804887c555dc6d8621ef9ca57424cf3
Deirdre Connolly <deirdre@zfnd.org>
2021-01-23 06:33:49 -0500

    Finish groth16 batch verification math

--- src/groth16/tests/dummy_engine.rs
+++ src/groth16/tests/dummy_engine.rs
@@ -342,7 +342,7 @@ impl Engine for DummyEngine {
     type Gt = Fr;
 
     fn pairing(p: &Self::G1Affine, q: &Self::G2Affine) -> Self::Gt {
-        Self::multi_miller_loop(&[(p, &(*q).into())]).final_exponentiation()
+        Self::multi_miller_loop(&[(p, &(*q))]).final_exponentiation()
     }
 }
 
--- src/groth16/verifier/batch.rs
+++ src/groth16/verifier/batch.rs
@@ -1,10 +1,7 @@
-use std::{
-    iter,
-    ops::{AddAssign, Neg},
-};
+use std::ops::AddAssign;
 
 use ff::Field;
-use group::{cofactor::CofactorCurveAffine, Curve, Group};
+use group::{Curve, Group};
 use pairing::{MillerLoopResult, MultiMillerLoop};
 use rand_core::{CryptoRng, RngCore};
 
@@ -55,41 +52,68 @@ where
         self.items.push(item.into())
     }
 
+    /// Verify a batch of (proof, inputs) items with a particular
+    /// [`PreparedVerifyingKey`].
+    ///
+    /// In practice, you would create a Spend batch verifier and an Output batch
+    /// verifier, add all the (Spend, inputs)'s and (Output, inputs)'s from a
+    /// block, and invoke this method on each, with the appropriate
+    /// [`PreparedVerifyingKey`].
+    ///
+    ///
     #[allow(non_snake_case)]
     pub fn verify<R: RngCore + CryptoRng>(
         self,
         mut rng: R,
-        pvk: &PreparedVerifyingKey<E>,
+        vk: &VerifyingKey<E>,
     ) -> Result<(), VerificationError> {
         let mut ml_terms = Vec::<(E::G1Affine, E::G2Prepared)>::new();
-        let mut acc_Gammas = vec![E::Fr::zero(); pvk.ic.len()];
+        let mut acc_Gammas = vec![E::Fr::zero(); vk.ic.len()];
         let mut acc_Delta = E::G1::identity();
         let mut acc_Y = E::Fr::zero();
 
         for Item { proof, inputs } in self.items.into_iter() {
             let z = E::Fr::random(&mut rng);
-            if inputs.len() + 1 != pvk.ic.len() {
+
+            if inputs.len() + 1 != vk.ic.len() {
                 return Err(VerificationError::InvalidVerifyingKey);
             }
+
+            ml_terms.push(((proof.a * z).into(), (-proof.b).into()));
+
             acc_Gammas[0] += &z; // a_0 is implicitly set to 1 (??)
             for (a_i, acc_Gamma_i) in Iterator::zip(inputs.iter(), acc_Gammas.iter_mut().skip(1)) {
                 *acc_Gamma_i += &(z * a_i);
             }
-            acc_Delta += proof.c * &z;
+            acc_Delta += proof.c * z;
             acc_Y += &z;
-            ml_terms.push(((proof.a * &z).into(), (-proof.b).into()));
         }
 
-        /*
-        let Psi = pvk
+        ml_terms.push((acc_Delta.to_affine(), E::G2Prepared::from(vk.delta_g2)));
+
+        let Psi = vk
             .ic
             .iter()
             .zip(acc_Gammas.iter())
             .map(|(&Psi_i, acc_Gamma_i)| Psi_i * acc_Gamma_i)
             .sum();
-            */
 
-        unimplemented!();
+        ml_terms.push((E::G1Affine::from(Psi), E::G2Prepared::from(vk.gamma_g2)));
+
+        // Y = PreparedVerifyingKey.alpha_g1_beta_g2
+        //
+        // Y^acc_Y in multiplicative notation is [acc_Y]Y (scalar mul) in
+        // additive notation, which is the code convention here.
+        let Y = E::pairing(&vk.alpha_g1, &vk.beta_g2) * acc_Y;
+
+        let mut terms = vec![];
+        ml_terms.iter().for_each(|(a, b)| terms.push((a, b)));
+
+        if E::multi_miller_loop(terms.as_slice()).final_exponentiation() + Y == E::Gt::identity() {
+            Ok(())
+        } else {
+            Err(VerificationError::InvalidProof)
+        }
     }
 }
 
